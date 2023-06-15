16f9dc42b50e3546ee04961fb2c6d8fc352909f8
Mehedi Hasan <mhjibon.info@gmail.com>
2023-06-15 16:57:32 +0600

    pass dynamic data to components, props in react

--- src/App.js
+++ src/App.js
@@ -51,6 +51,9 @@ function App() {
         <h5> New component. YAY </h5>
         <p> Rock mama React mama. </p>
 
+        <Friend movie="shingam" phone="017777"></Friend>
+        <Friend phone="019999"></Friend>
+
     </div>
   );
 }
@@ -63,6 +66,18 @@ function Person (props){
       <h1> {props.name}</h1>
       {/* <p> Profession: Cricket </p> */}
       {/* <p> Nayika; {props.nayika}</p> */}
+    </div>
+  )
+}
+
+function Friend (props){
+  // console.log(props);
+  return(
+    <div className='container'>
+      {/* <h3> Name: Ajay Devgun</h3> */}
+      {/* <p> job: maramari </p> */}
+      <h3> Name: {props.movie}</h3>
+      <p> Phone: {props.phone}</p>
     </div>
   )
 }
